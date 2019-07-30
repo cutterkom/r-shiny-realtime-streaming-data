a4a057860aea443d33aad2e6b9fa3c38d50820a4
Katharina Brunner <mail@katharina-brunner.de>
2019-07-30 16:26:47 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# r-shiny-realtime-streaming-data
- This is a template for streaming real time data into an R-Shiny webapp. It includes working examples for ggplot, plotly and scatterD3.
+# R-Shiny Template for real time data
+
+This is a very basic template for streaming realtime data in [R-Shiny](https://shiny.rstudio.com/). 
+
+It has two components: 
+
+1. Generating a data point every two seconds
+
+2. Plotting the data with three packages: [ggplot2](https://ggplot2.tidyverse.org/), [plotly](https://plot.ly/) and [scatterD3](https://juba.github.io/scatterD3/)
+
+
+## 1) Generating new data
+
+First, I initialize an empty dataframe as a [``reactiveValues`` object](https://shiny.rstudio.com/articles/reactivity-overview.html). It receives and stores the new data points.
+
+````
+# initialise an empty dataframe as a reactiveValues object.
+# it is going to store all upcoming new data
+
+values <- reactiveValues(df = data.frame(x = NA, y = NA))
+```
+
+Then, random data is saved in a `reactive values` dataframe. This `values$df` will be incrementally filled every two seconds by using [`observeEvent()`](https://shiny.rstudio.com/reference/shiny/1.0.0/observeEvent.html):
+
+````
+ 
+observeEvent(reactiveTimer(2000)(),{ # Trigger every 2 seconds
+    values$df <- isolate({
+      # get and bind the new data
+      values_df <- rbind(values$df, get_new_data()) %>% filter(!is.na(x))
+    })
+  })
+````
+
+## 2) Plotting the data
+
+After receiving new data in the `reactive values` dataframe, the app plots the data with three packages. 
+
+The `ggplot2` example:
+
+```
+  # create ggplot2 chart 
+  output$plot <- renderPlot({
+    
+    x_axis_start <- as.POSIXct(min(values$df$x), format = "%Y-%m-%d", origin="1970-01-01")
+    x_axis_end <-  as.POSIXct(min(values$df$x), format = "%Y-%m-%d", origin="1970-01-01") + 1000
+    y_axis_range <- c(0, 70)
+    
+  values$df %>% 
+  ggplot(aes(x = as.POSIXct(x, format = "%Y-%m-%d", origin="1970-01-01"), y = y)) + 
+  geom_point() + 
+  expand_limits(x = c(x_axis_start, x_axis_end), y = y_axis_range)
+})
+```
---
+++ app.R
@@ -0,0 +1,107 @@
+library(shiny)
+library(tidyverse)
+library(plotly)
+library(scatterD3)
+
+
+ui <- shinyServer(fluidPage(
+  
+  h1("Template for plotting real-time data in R with Shiny"),
+  
+  p("Task: Every two seconds, plot a new random point."),
+  p("It is accomplished by combining `reactiveValues()`, an object that collects the existing and incoming data, with `observeEvent()`, that executes data fetching proccess every two seconds."
+  ),
+  a(href = "https://github.com/cutterkom/r-shiny-realtime-streaming-data", "More information in the Readme file on Github."),
+  
+  
+  h2("ggplot2 Library"),
+  plotOutput("plot"),
+  
+  h2("Plotly Library"),
+  plotlyOutput("plotly"),
+  
+  h2("D3"),
+  scatterD3Output("d3")
+  
+))
+
+
+server <- shinyServer(function(input, output, session) {
+  # helper: shut down Shiny session in RStudio, when you close the browser tab
+  session$onSessionEnded(stopApp)
+  
+  # function to create a dataframe with column x as a random number and y as the current timestamp
+  get_new_data <- function() {
+    y = sample(10:70, 1) # draw a random number between 10 and 70
+    x = as.POSIXct(Sys.time(), format = "%Y-%m-%d %H%m", origin = "1970-01-01")
+    data.frame(x, y)
+  }
+  
+  # initialise an empty dataframe as a reactiveValues object.
+  # it is going to store all upcoming new data
+  values <- reactiveValues(df = data.frame(x = NA, y = NA))
+  
+  # call the function get_new_data() every two seconds
+  # and bind the resulting dataframe to the reactiveValues dataframe
+  observeEvent(reactiveTimer(2000)(), {
+    # Trigger every 2 seconds
+    values$df <- isolate({
+      # get and bind the new data
+      rbind(values$df, get_new_data()) %>%
+        filter(!is.na(x)) # filter the first value to prevent a first point in the middle of the plot
+    })
+  })
+  
+  # create ggplot2 chart
+  output$plot <- renderPlot({
+    x_axis_start <-
+      as.POSIXct(min(values$df$x), format = "%Y-%m-%d", origin = "1970-01-01")
+    x_axis_end <-
+      as.POSIXct(min(values$df$x), format = "%Y-%m-%d", origin = "1970-01-01") + 1000
+    y_axis_range <- c(0, 70)
+    
+    ggplot(data = values$df, aes(
+      x = as.POSIXct(x, format = "%Y-%m-%d", origin = "1970-01-01"),
+      y = y
+    )) +
+      geom_point() +
+      expand_limits(x = c(x_axis_start, x_axis_end), y = y_axis_range)
+  })
+  
+  # create plotly chart
+  output$plotly <- renderPlotly({
+    x_axis_start <-
+      as.POSIXct(min(values$df$x), format = "%Y-%m-%d", origin = "1970-01-01")
+    x_axis_end <-
+      as.POSIXct(min(values$df$x), format = "%Y-%m-%d", origin = "1970-01-01") + 1000
+    
+    
+    plot_ly(
+      data = values$df,
+      x = ~ as.POSIXct(x, format = "%Y-%m-%d", origin = "1970-01-01"),
+      y = ~ y,
+      type = "scatter",
+      mode = "markers"
+    ) %>%
+      layout(xaxis = list(
+        range =  c(x_axis_start, x_axis_end),
+        type = "date"
+      ),
+      yaxis = list(range = c(0, 70)))
+    
+  })
+  
+  # create D3 chart
+  output$d3 <- renderScatterD3({
+    scatterD3(
+      data = values$df,
+      x = x,
+      y = y,
+      xlim = c(min(values$df$x), min(values$df$x) + 1000),
+      ylim = c(0, 70)
+    )
+  })
+  
+})
+
+shinyApp(ui = ui, server = server)
